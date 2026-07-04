b0a0f6238e19fa00089efa756439ca9f9d562404
Heather Vandervecht <heathervandervecht@gmail.com>
2026-07-04 10:48:20 -0400

    feat: higher power

---
+++ src/components/sections/HigherPower.tsx
@@ -0,0 +1,63 @@
+import List from "../elements/List"
+import Number from "../elements/Number"
+import Text from "../elements/Text"
+import WithTooltip from "../elements/WithTooltip"
+
+const BASE_KEY = 'higher_power_'
+
+const HigherPower = () => (
+    <section>
+        <section className="flex flex-col gap-2 md:flex-row">
+            <Text id={`${BASE_KEY}source`} label="Source" />
+            <Text id={`${BASE_KEY}path`} label="Path" />
+            <Text id={`${BASE_KEY}archetype`} label="Archetype" />
+        </section>
+        <hr className="mt-4 mb-4 border-base-content/10" />
+        <section>
+            <WithTooltip text="Once per weeks rest you may ask for one miracle from your higher power source, which will be provided if it's a reasonable request aligned with their ideals, and will replenish on a week's rest if your deity is pleased with you.">
+                <p className="text-lg font-bold inline-block mb-2">Miracles</p>
+            </WithTooltip>
+            <List id={`${BASE_KEY}miracles`} />
+        </section>
+        <hr className="mt-4 mb-4 border-base-content/10" />
+        <section>
+            <p className="text-lg font-bold mb-2">Dice</p>
+            <div className="flex flex-col lg:flex-row lg:items-center gap-2 lg:gap-4">
+                <div className="flex flex-col md:flex-row lg:flex-col flex-1 gap-2">
+                    <Number
+                        id={`${BASE_KEY}available_dice`}
+                        label="Available Dice"
+                        tooltip="Dice can be reused until they decay. If your decay is 2, rolling a 2 will cause the dice to drop from the pool until the pool is restored. Dice pools will recover after a week’s rest."
+                    />
+                    <Number id={`${BASE_KEY}total_die_pool`} label="Total Die Pool" />
+                </div>
+                <div className="flex flex-col md:flex-row lg:flex-col flex-1 gap-2">
+                    <Number id={`${BASE_KEY}temporary_dice`} label="Temporary Dice" />
+                    <Number
+                        id={`${BASE_KEY}decay`}
+                        label="Decay"
+                    />
+                </div>
+            </div>
+        </section>
+        <hr className="mt-4 mb-4 border-base-content/10" />
+        <section>
+            <p className="block text-lg font-bold mb-2">Boons</p>
+            <p className="text-md font-bold inline-block mb-2">Primary Boons</p>
+            <List id={`${BASE_KEY}primary_boons`} />
+            <span className="block mb-4" />
+            <p className="text-md font-bold inline-block mb-2">Secondary Boons</p>
+            <List id={`${BASE_KEY}secondary_boons`} />
+            <span className="block mb-4" />
+            <p className="text-md font-bold inline-block mb-2">Tertiary Boons</p>
+            <List id={`${BASE_KEY}tertiary_boons`} />
+        </section>
+        <hr className="mt-4 mb-4 border-base-content/10" />
+        <section>
+            <p className="text-lg font-bold inline-block mb-2">Corruptions</p>
+            <List id={`${BASE_KEY}corruptions`} />
+        </section>
+    </section>
+)
+
+export default HigherPower
--- src/data/sections.ts
+++ src/data/sections.ts
@@ -2,16 +2,19 @@ import Character from "../components/sections/Character"
 import SocialSkills from "../components/sections/SocialSkills"
 import PhysicalSkills from "../components/sections/PhysicalSkills"
 import MagicSkills from "../components/sections/MagicSkills"
+import HigherPower from "../components/sections/HigherPower"
 import IconMagic from "../assets/images/icon-magic"
 import IconPhysical from "../assets/images/icon-physical"
 import IconSocial from "../assets/images/icon-social"
 import IconCharacter from "../assets/images/icon-character"
+import IconMeditate from "../assets/images/icon-meditate"
 
 export enum SECTIONS {
     CHARACTER = 'character',
     SOCIAL_SKILLS = 'social_skills',
     PHYSICAL_SKILLS = 'physical_skills',
     MAGIC_SKILLS = 'magic_skills',
+    HIGHER_POWER = 'higher_power',
 }
 
 export const section_titles = {
@@ -19,6 +22,7 @@ export const section_titles = {
     [SECTIONS.SOCIAL_SKILLS]: "Social Skills",
     [SECTIONS.PHYSICAL_SKILLS]: "Physical Skills",
     [SECTIONS.MAGIC_SKILLS]: "Magic Skills",
+    [SECTIONS.HIGHER_POWER]: "Higher Power",
 }
 
 export const section_icons = {
@@ -26,6 +30,7 @@ export const section_icons = {
     [SECTIONS.SOCIAL_SKILLS]: IconSocial,
     [SECTIONS.PHYSICAL_SKILLS]: IconPhysical,
     [SECTIONS.MAGIC_SKILLS]: IconMagic,
+    [SECTIONS.HIGHER_POWER]: IconMeditate,
 }
 
 export const section_components = {
@@ -33,4 +38,5 @@ export const section_components = {
     [SECTIONS.SOCIAL_SKILLS]: SocialSkills,
     [SECTIONS.PHYSICAL_SKILLS]: PhysicalSkills,
     [SECTIONS.MAGIC_SKILLS]: MagicSkills,
+    [SECTIONS.HIGHER_POWER]: HigherPower,
 }
